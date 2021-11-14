d066f4474f751896fe435b0b0c31b0c4fe9c05e5
Jackson Gunther <jackson.gunther@yahoo.com>
2021-11-14 10:52:02 -0700

    need to figure out double for loop stuff

--- pyramid.py
+++ pyramid.py
@@ -1,15 +1,17 @@
+import sys
 def weight_on(row, column):
     second_person = 200 #finds out if there is a second person on top or not
     if column - 1 < 0 or column > row - 1 :
         second_person = 0
     if column < 0 or column > row:
-        return 0
+        return 0.0
     if row <= 0:
-        return 0
+        return 0.0
     return (second_person + 200 + (weight_on(row - 1, column - 1) + weight_on(row - 1, column))) / 2
 
 def main():
-    print(weight_on(3, 1))
+    for i in range(int(sys.argv[1])):
+        print(weight_on(i, i))
 
 if __name__ == '__main__':
     main()
