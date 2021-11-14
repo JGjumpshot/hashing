8d48a2777e012e426bddc0d29d703d5cad3c4a58
Jackson Gunther <jackson.gunther@yahoo.com>
2021-11-13 18:07:49 -0700

    updates

--- pyramid.py
+++ pyramid.py
@@ -1,12 +1,15 @@
 def weight_on(row, column):
+    second_person = 200 #finds out if there is a second person on top or not
+    if column - 1 < 0 or column > row - 1 :
+        second_person = 0
+    if column < 0 or column > row:
+        return 0
     if row <= 0:
         return 0
-    if column <= 0 or column > row:
-        return 0
-    return 100 + (weight_on(row - 1, column - 1) + weight_on(row - 1, column)) / 2
+    return (second_person + 200 + (weight_on(row - 1, column - 1) + weight_on(row - 1, column))) / 2
 
 def main():
-    print(weight_on(3, 2))
+    print(weight_on(3, 1))
 
 if __name__ == '__main__':
     main()
