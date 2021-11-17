a7cf344a00a403cfd850f5b9df9fba0630e947ae
Jackson Gunther <jacksongunther@filevine.com>
2021-11-16 20:34:37 -0700

    updates

--- pyramid.py
+++ pyramid.py
@@ -1,6 +1,6 @@
 import sys
 def weight_on(row, column):
-    second_person = 200 #finds out if there is a second person on top or not
+    second_person = 200
     if column - 1 < 0 or column > row - 1 :
         second_person = 0
     if column < 0 or column > row:
@@ -10,14 +10,16 @@ def weight_on(row, column):
     return (second_person + 200 + (weight_on(row - 1, column - 1) + weight_on(row - 1, column))) / 2
 
 def main():
-    argument = sys.argv[1]
+    argument = int(sys.argv[1])
     if (len(sys.argv) <= 1):
         raise IndexError("Please enter a valid argument")
         
-    for i in range(int(argument)):
-        for j in range(i):
+    for i in range(argument):
+        print(i, end=' ')
+        if i == 0:
+            print(f"{0:.2f}", end=' ')
+        for j in range(i + 1):
             print(f"{weight_on(i, i):.2f}", end=' ')
-            # print(i, end=' ')
         print('')
 if __name__ == '__main__':
     main()
