41d2a8722acf118db7592d0ccbc8740ecb6a7176
Jackson Gunther <jacksongunther@filevine.com>
2021-11-17 20:19:23 -0700

    part 2 complete

--- pyramid.py
+++ pyramid.py
@@ -4,15 +4,13 @@ def weight_on(row, column):
     global counter
     counter += 1
     second_person = 200
-    # if column == 0:
-    #     return weight_on(row - 1, column - 1)
-    if column - 1 < 0 or column > row - 1 :
-        second_person = 0
-    if column < 0 or column > row:
-        return 0.0
-    if row <= 0:
-        return 0.0
-    return (second_person + 200 + (weight_on(row - 1, column - 1) + weight_on(row - 1, column))) / 2
+    if column == 0 and row == 0:
+        return 0
+    if column == 0:
+        return second_person / 2 + weight_on(row - 1, 0) / 2
+    if column == row:
+       return second_person / 2 + weight_on(row - 1, column - 1) / 2
+    return (second_person + (weight_on(row - 1, column - 1) / 2 + weight_on(row - 1, column) / 2)) 
 
 def main():
     argument = int(sys.argv[1])
