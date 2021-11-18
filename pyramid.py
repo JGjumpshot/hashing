bc662ea5cce6d67ed35e33ede977fcd3962514e0
Jackson Gunther <jackson.gunther@yahoo.com>
2021-11-17 19:27:54 -0700

    updates

--- pyramid.py
+++ pyramid.py
@@ -1,6 +1,11 @@
 import sys
+counter = 0
 def weight_on(row, column):
+    global counter
+    counter += 1
     second_person = 200
+    # if column == 0:
+    #     return weight_on(row - 1, column - 1)
     if column - 1 < 0 or column > row - 1 :
         second_person = 0
     if column < 0 or column > row:
@@ -10,7 +15,6 @@ def weight_on(row, column):
     return (second_person + 200 + (weight_on(row - 1, column - 1) + weight_on(row - 1, column))) / 2
 
 def main():
-    # print(weight_on(3, 1))
     argument = int(sys.argv[1])
     if (len(sys.argv) <= 1):
         raise IndexError("Please enter a valid argument")
@@ -19,6 +23,7 @@ def main():
         for j in range(i + 1):
             print(f"{weight_on(i, j):.2f}", end=' ')
         print('')
+    print(counter)
 if __name__ == '__main__':
     main()
     
