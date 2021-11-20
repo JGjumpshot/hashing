49b111d3dea7955ca4ab374504b7b2f6261861f1
Jackson Gunther <jackson.gunther@yahoo.com>
2021-11-20 12:15:27 -0700

    updates

--- hashmap.py
+++ hashmap.py
@@ -5,38 +5,21 @@ class HashMap:
         self._size = 7
         self._capacity = 7
         self.arr = [None for index in range(0, self._size)]
-        print(self.arr)
     def hash_function(self, key):
         #self.size = 7
         hashed_key = key[0] + key[1] % self._size
         return hashed_key
+    def set(self, key, val):
+        h = self.hash_function(key)
+        self.arr[h] = val
+    def get(self, key):
+        h = self.hash_function(key)
+        return self.arr[h]
+    def remove(self, key):
+        h = self.hash_function(key)
+        self.arr[h] = None
     def size(self):
         return 0
-    def set(self, key, value):
-        hash_key = self.hash_function(key)
-        key_exists = False
-        slot = self.arr[hash_key]
-        slot_value = 0
-        for i, kv in enumerate(slot):
-            k, v = kv
-            if key == k:
-                key_exists = True
-                slot_value = i
-                break
-        if key_exists:
-            slot[slot_value] = ((key, value))
-        else:
-            slot.append((key,value))
-    def get(self, key):
-        hash_key = self.hash_function(key)
-        slot = self.arr[hash_key]
-        for kv in slot:
-            k, v = kv
-            if key == k:
-                return v
-        raise KeyError("key doesn't exist")
-    def capacity(self):
-        return self._capacity
 key = (0, 0)
 value = 27
 
@@ -49,4 +32,6 @@ hm = HashMap(key, value)
 
 hm.set(key2, value2)
 # print(hm.get(key2))
+print(hm.arr)
+hm.remove(key2)
 print(hm.arr)
