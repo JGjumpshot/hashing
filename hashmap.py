694ad92bf00c5bd55225e5ef223a7a1442ec96ac
Jackson Gunther <jackson.gunther@yahoo.com>
2021-11-21 21:53:05 -0700

    latest and greatest

--- hashmap.py
+++ hashmap.py
@@ -1,9 +1,10 @@
+import pytest
 class HashMap:
     def __init__(self, key=None, value=None):
         self.key = key
         self.value = value
         self._size = 0
-        self._capacity = 20
+        self._capacity = 7
         # self._input = 0
         self.arr = [[] for index in range(0, self._capacity)]
     DEFAULT_LOAD_FACTOR = 0.80
@@ -31,47 +32,18 @@ class HashMap:
             print("Therefore Rehashing will be done.")
  
             # Rehash
-            self.rehash()
+            self.rehash(h, self._capacity)
  
-            print("New Size of Map: " + str(self._size))
+            print("New Size of Map: " + str(self._capacity))
  
         print("Number of pairs in the Map: " + str(self._size))
         print("Size of Map: " + str(self._size))
         # self.arr[h] = val
-    def rehash(self):
-        print("\n***Rehashing Started***\n")
- 
-        # The present bucket list is made temp
-        temp = self.arr
- 
-        # New bucketList of double the old size is created
-        slots = (2 * len(self.arr))
-        print(slots)
-        for i in range(2 * self._capacity - 1):
-            # Initialised to null
-            temp.append([])
- 
-        # Now size is made zero
-        # and we loop through all the nodes in the original bucket list(temp)
-        # and insert it into the new list
-        self._size = 0
-        self._capacity *= 2
- 
-        for i in range(len(temp)):
- 
-            # head of the chain at that index
-            head = temp[i]
- 
-            while (head != []):
-                key = head[i][0]
-                val = head[i][1]
- 
-                # calling the insert function for each node in temp
-                # as the new list is now the bucketArray
-                self.set(key, val)
-                head = temp[i + 1]
- 
-        print("\n***Rehashing Ended***")
+    def rehash(self, oldhash, size):
+        new_size = (size * 2) - 1
+        self._capacity = new_size
+        self.arr.append(self._capacity)
+        return (oldhash * 2) - 1 % self._capacity
     def get(self, key):
         h = self.hash_function(key)
         if h > 0:
@@ -98,37 +70,22 @@ class HashMap:
 # key = (0, 0)
 # value = 27
 
-key2 = (3, 2)
-value2 = 44
+# key2 = (3, 2)
+# value2 = 44
 
-key3 = (4, 3)
-value3 = 77
-
+# key3 = (4, 3)
+# value3 = 77
+hm = HashMap()
+keys = [(r,r) for r in (range(10))]
+values = list(range(1, 11))
+for k,v in zip(keys,values):
+    hm.set(k,v)
+hm.remove((3,3))
+print(hm)
+with pytest.raises(KeyError):
+    hm.get((3,3))
+assert hm.get((5,5)) == 6
 # key3 = (3, 2)
 # value3 = 55
 # hm = HashMap(key, value)
 
-# hm.set(key2, value2)
-# # print(hm.get(key2))
-# print(hm.arr)
-# hm.remove(key2)
-# print(hm.arr)
-hm = HashMap()
-# print(hm.arr)
-# hm.set(key2, value2)
-# hm.set(key3, value3)
-# hm.set((5,4), 23)
-# hm.set((2,1), 71)
-# hm.set((2,2), 12)
-# hm.set((2,0), 34)
-# print(hm.size)
-# print(hm.arr)
-# print(hm.get(key2))
-# hm.remove(key2)
-# hm.remove(key3)
-print(hm.get((3,3)))
-# keys = [(r,r) for r in (range(10))]
-# print(keys)
-# values = list(range(1, 11))
-# for k,v in zip(keys,values):
-#     hm.set(k,v)
